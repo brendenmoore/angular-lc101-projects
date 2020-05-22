f9b72ef985675307520b437416bb018bcbdddec3
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-05-21 21:06:58 -0400

    Lesson 2 complete

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: "movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent implements OnInit {
-   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+  movies = ["Toy Story", "The Shining", "Sleepless in Seattle", "The Martian"];
+  error = {
+    message: "",
+    class: "",
+  };
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
+  ngOnInit() {}
+
+  addMovie(newTitle: string) {
+    if (this.movies.includes(newTitle)) {
+      this.error.message = "Please pick a different movie!";
+      this.error.class = "orange";
+    } else if (newTitle === "") {
+      this.error.message = "Please enter a title";
+      this.error.class = "red";
+    } else {
+      this.movies.push(newTitle);
+      this.error.message = "";
+    }
+  }
 }
